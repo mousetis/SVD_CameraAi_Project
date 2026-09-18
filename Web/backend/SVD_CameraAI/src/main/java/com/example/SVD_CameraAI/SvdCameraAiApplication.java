b599a5d8db1019856df82866c5c80e87991a71fd
mousetis <chitoanphan8505@gmail.com>
2026-09-18 18:41:27 +0700

    init second time

---
+++ Web/backend/SVD_CameraAI/src/main/java/com/example/SVD_CameraAI/SvdCameraAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SVD_CameraAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvdCameraAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvdCameraAiApplication.class, args);
+	}
+
+}
---
+++ Web/backend/SVD_CameraAI/src/test/java/com/example/SVD_CameraAI/SvdCameraAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SVD_CameraAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvdCameraAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
